28b65a66a05ee0636b44ee8cf2b54bd535c32d9f
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-10-18 20:07:52 +0300

    #5 Project name was changed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const gitHubApiUrl = 'https://api.github.com';
+export const projectName = 'Andrei Arkhipov';
 export const authorAccount = 'awibox';
 export const repoName = 'react-redux-app-boilerplate';
 export const UserArray = [authorAccount, 'angular', 'facebook', 'reduxjs', 'webpack'];
