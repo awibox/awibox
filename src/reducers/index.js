70da7cb353331678c963457683a3a7494946b0a3
Andrei Arkhipov <wibox@ya.ru>
2019-10-20 20:19:28 +0300

    #2 Remove unused content

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux';
 // Reducers
-import authorReducer from './authorReducer';
 import errorReducer from './errorReducer';
 import homeReducer from './homeReducer';
-import reposReducer from './reposReducer';
 
 export default combineReducers({
-  authorState: authorReducer,
   errors: errorReducer,
   homeState: homeReducer,
-  reposState: reposReducer,
 });
