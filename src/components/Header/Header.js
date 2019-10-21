2b0fc187c467092352d7f1c5b2c5515202527f8b
Andrei Arkhipov <wibox@ya.ru>
2019-10-21 16:15:35 +0300

    #4 New logo for site

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,9 +9,7 @@ import styles from './Header.scss';
 const Header = () => (
   <header className={styles.header}>
     <Link className={styles.logoBlock} to="/">
-        <span className={styles.logo}>
-            <i className="fa fa-gears"></i>
-        </span>
+        <span className={styles.logo}></span>
       <span className={styles.name}>{projectName}</span>
     </Link>
     <NavBar/>
