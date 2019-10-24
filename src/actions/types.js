186db18ca50f5953d51e57cb53ccfe65d02ebf3e
Andrei Arkhipov <wibox@ya.ru>
2019-10-24 12:17:37 +0300

    #23 Books component was added

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
 export const GET_ERRORS = 'GET_ERRORS';
 export const GET_WORK_DATA = 'GET_WORK_DATA';
 export const GET_WORK_SKILLS = 'GET_WORK_SKILLS';
+export const GET_WORK_BOOKS = 'GET_WORK_BOOKS';
