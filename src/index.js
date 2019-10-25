788762423fe454fc27e6df3c7fa7535159d96da4
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-10-25 17:07:20 +0300

    #40 Google Analytics was added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import store from './store';
 import RouterComponent from './router';
 // Styles
@@ -8,3 +9,5 @@ import 'styles/build.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<Provider store={store}><RouterComponent/></Provider>, document.getElementById('root'));
+ReactGA.initialize('UA-44249492-4');
+ReactGA.pageview(window.location.pathname + window.location.search);
