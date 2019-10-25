df4b70eeb99ccb30c0d817aa4ec87d06d9bd7606
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-10-25 18:58:04 +0300

    Revert "#40 Google Analytics was added"
    This reverts commit 2b46a47f44a6c14fa26d9bed40cab8232c401730.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
 import store from './store';
 import RouterComponent from './router';
 // Styles
@@ -9,5 +8,3 @@ import 'styles/build.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<Provider store={store}><RouterComponent/></Provider>, document.getElementById('root'));
-ReactGA.initialize('UA-44249492-4');
-ReactGA.pageview(window.location.pathname + window.location.search);
