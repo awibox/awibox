be16a8bf06f44794657f9ed1321319c71e5349fd
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-10-28 21:18:59 +0300

    #46 Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// Styles
+import styles from './Footer.scss';
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div>
+      <strong>Copyright</strong> awibox © 2019
+    </div>
+  </footer>);
+
+export default Footer;
