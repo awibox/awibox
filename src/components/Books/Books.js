309e562cae8fcf7585b67b154f2e6fe021688dd5
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-10-31 21:22:03 +0300

    #51 hint for books was added

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -8,7 +8,10 @@ const bookClick = (url) => window.open(url);
 const Books = (props) => (
   <div className={styles.books}>
     {props.data.map((book) => (
-      <div key={book.name} className={styles.bookItem} onClick={() => bookClick(book.link)}>
+      <div key={book.name}
+           title={book.name}
+           className={styles.bookItem}
+           onClick={() => bookClick(book.link)}>
         <div className={styles.bookName}>{book.name}</div>
         <div className={styles.author}>Author: <b>{book.author}</b></div>
       </div>
