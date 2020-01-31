cd6f4b0d636468d0cb176da320293a656ebd7772
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2020-01-31 22:52:48 +0300

    #105: react-cookie-consent-notification was added

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Components
 import Header from 'components/Header/Header';
@@ -6,26 +6,57 @@ import Footer from 'components/Footer/Footer';
 // Pages
 import ProfileContainer from 'containers/Profile/Profile';
 import NotFound from 'pages/NotFound/NotFound';
+import CookiePolicy from 'pages/CookiePolicy/CookiePolicy';
 // Styles
 import styles from 'styles/container.scss';
+import CookieConsent from 'react-cookie-consent-notification';
 
-export const routes = {};
+class RouterComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      consentStatus: false,
+    };
+    this.checkStatus = this.checkStatus.bind(this);
+  }
 
-const RouterComponent = () => (
-  <Router>
-    <div className={styles.app}>
-      <Header/>
-      <main className={styles.content}>
-        <div className={styles.wrapper}>
-          <Switch>
-            <Route exact path="/" component={ProfileContainer}/>
-            <Route exact path="/test" component={ProfileContainer}/>
-            <Route component={NotFound}/>
-          </Switch>
+  checkStatus(consentStatus) {
+    this.setState({ consentStatus });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className={styles.app}>
+          <Header/>
+          <main className={styles.content}>
+            <div className={styles.wrapper}>
+              <Switch>
+                <Route exact path="/" component={ProfileContainer}/>
+                <Route exact path="/cookie-policy" component={CookiePolicy}/>
+                <Route component={NotFound}/>
+              </Switch>
+            </div>
+          </main>
+          <CookieConsent
+            background={'#0079c1'}
+            bottomPosition={false}
+            buttonText={'I agree'}
+            buttonBackground={'#fff'}
+            buttonColor={'#000'}
+            buttonFontSize={14}
+            color={'#fff'}
+            consentFunction={this.checkStatus}
+            padding={20}
+          >
+            This website uses cookies for analytical purposes.
+            Please read our <a href={'/cookie-policy'} style={{ color: '#fff' }}>Cookie Policy</a> and confirm your consent to the use of cookies.
+          </CookieConsent>
+          <Footer/>
         </div>
-      </main>
-      <Footer/>
-    </div>
-  </Router>
-);
+      </Router>
+    );
+  }
+}
+
 export default RouterComponent;
