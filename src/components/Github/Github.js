14ef63a9b02f0459ce137ff5b91cf1cbdfe0ddfb
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2021-03-19 22:02:18 +0300

    #365: atl for img

--- src/components/Github/Github.js
+++ src/components/Github/Github.js
@@ -5,7 +5,7 @@ import styles from './Github.scss';
 const Github = () => (
   <div className={styles.github}>
     <img className={styles.stats} src="https://github-readme-stats.vercel.app/api?username=awibox&show_icons=true&hide_title=true&icon_color=fea621&title_color=fea621&hide_border=true&include_all_commits=true&count_private=true&hide_rank=false" alt='GitHub Stats' />
-    <img className={styles.languages} src="https://github-readme-stats.vercel.app/api/top-langs/?username=awibox&layout=compact&langs_count=8&hide_border=true&hide_title=true" />
+    <img className={styles.languages} src="https://github-readme-stats.vercel.app/api/top-langs/?username=awibox&layout=compact&langs_count=8&hide_border=true&hide_title=true" alt='Most used languages' />
   </div>
 );
 
