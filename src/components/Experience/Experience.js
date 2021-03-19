ff214b8e416ea6786996eb305b84a5982d84edc1
Andrei Arkhipov <me@awb.pw>
2021-03-20 02:18:23 +0300

    #369: Experience logic

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -11,10 +11,10 @@ const Experience = (props) => (
       <div key={item.name} className="col-md-6">
         <div className="skill clearfix">
           <h4>{item.name}</h4>
-          <div className={styles.skillValue}>{item.years} years</div>
+          <div className={styles.skillValue}>{item.years} {item.years === 1 ? 'year' : 'years'}</div>
         </div>
         <div className={styles.skillContainer}>
-          <div className={styles.skillPercentage}></div>
+          <div className={styles.skillPercentage} style={{ width: `${item.percentage}%` }}></div>
         </div>
       </div>
     ))}
