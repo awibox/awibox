1dee0c4f497eab3faba3b2236bb3620d741b7da3
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2021-03-19 21:56:11 +0300

    #365: styles for github parts were added

--- src/components/Github/Github.js
+++ src/components/Github/Github.js
@@ -4,8 +4,8 @@ import styles from './Github.scss';
 
 const Github = () => (
   <div className={styles.github}>
-    <img align="center" src="https://github-readme-stats.vercel.app/api?username=awibox&show_icons=true&hide_title=true&icon_color=fea621&title_color=fea621&hide_border=true&include_all_commits=true&count_private=true&hide_rank=false" alt='GitHub Stats' />
-    <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=awibox&layout=compact&langs_count=8&hide_border=true&hide_title=true" />
+    <img className={styles.stats} src="https://github-readme-stats.vercel.app/api?username=awibox&show_icons=true&hide_title=true&icon_color=fea621&title_color=fea621&hide_border=true&include_all_commits=true&count_private=true&hide_rank=false" alt='GitHub Stats' />
+    <img className={styles.languages} src="https://github-readme-stats.vercel.app/api/top-langs/?username=awibox&layout=compact&langs_count=8&hide_border=true&hide_title=true" />
   </div>
 );
 
